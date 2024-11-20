15fd74dd79969fd4e68e93e1b3263f5ffc6ecd04
Aryan Salaria <arayn.32@gmail.com>
2024-11-20 23:42:14 +0530

    Variables in php

---
+++ Intro/var1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+    $charname = "Joe";
+    $charage = 19;
+
+    echo "$charname was born in Chicago on a snowy evening <br>";
+    echo "$charname is $charage years old. <br>";
+    echo "$charname loves eating pizza! <br>";
+
+ ?>
+
+
+    
+</body>
+</html>
