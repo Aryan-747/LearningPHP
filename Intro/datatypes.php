b02c5a35d2649e3c731addf823cd53870cc72c9c
Aryan Salaria <arayn.32@gmail.com>
2024-11-20 23:57:08 +0530

    Basic data types

---
+++ Intro/datatypes.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+        $stringo = "Hello, how are you?"; // String
+        $numero = 10; // Int
+        $decimal = 10.5; // Decimal/Float
+        $ismale = false; //Boolean;
+        null; // Null
+
+        echo "String: $stringo <br>";
+        echo "Number: $numero <br>";
+        echo "Decimal: $decimal <br>";
+        echo "Boolean: $ismale <br>";
+
+    
+     ?>
+
+    
+</body>
+</html>
