0b9e63c7451d7bb550671cfcf9c19ee1f9fded9d
Aryan Salaria <arayn.32@gmail.com>
2024-11-24 23:39:16 +0530

    Updates

--- Intro/strfunc.php
+++ Intro/strfunc.php
@@ -12,10 +12,16 @@
         $stringo = "Hello Wassgud!";
 
         echo strtolower($stringo); // Converts Uppercase to Lowercase
+        echo "<br>";
         echo strtoupper($stringo); // Converts Lowercase to Uppercase
+        echo "<br>";
         echo ucfirst($stringo); // Converts First Letter to Uppercase
+        echo "<br>";
         echo ucwords($stringo); // Converts First Letter of Each Word to Uppercase
-    
+        echo "<br>";
+        echo strlen($stringo); // Prints the length of the string
+        echo "<br>";
+
      ?>
     
 </body>
