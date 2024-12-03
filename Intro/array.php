89f77a8a9c8a5e95afedca9fcf7fe90637b402e5
Aryan Salaria <arayn.32@gmail.com>
2024-12-03 12:23:14 +0530

    Simple arra iteration

---
+++ Intro/array.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <?php
+
+
+        $numbers = array(100,15,123,154,1233);
+
+        for($i=0 ; $i<count($numbers) ; $i++)
+        {
+            echo $numbers[$i];
+            echo "<br>";
+        }
+
+
+
+    ?>
+    
+</body>
+</html>
